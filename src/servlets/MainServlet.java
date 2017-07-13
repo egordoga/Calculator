c7a60e079c6b5b4766c8f038744c9f45b0bdceca
Игорь <edoga @ukr.net>
2017-07-13 17:43:22 +0300

    first commit

---
+++ src/servlets/MainServlet.java
@@ -0,0 +1,27 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "MainServlet")
+public class MainServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html; charset=utf-8");
+        HttpSession session = request.getSession();
+
+        PrintWriter out = response.getWriter();
+
+        out.println("<h2 style='color:blue'>ID сессии = " + session.getId() + "</h2>");
+    }
+}
